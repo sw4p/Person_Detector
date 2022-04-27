26f21d29d55aed867b9ee0a795288be10b6124eb
Sw4p <usav.swapnil@gmail.com>
2022-04-27 23:33:36 +0100

    Added CO2 level calculation. It is only calculating the CO2 added and not removed as of now.

--- piropo-open-mv-fw-v6-240x240/ei_object_detection.py
+++ piropo-open-mv-fw-v6-240x240/ei_object_detection.py
@@ -1,6 +1,6 @@
 # Edge Impulse - OpenMV Object Detection Example
 
-import sensor, image, time, os, tf, math, uos, gc
+import sensor, image, time, os, tf, math, uos, gc, pyb
 
 sensor.reset()                         # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.GRAYSCALE)    # Set pixel format to RGB565 (or GRAYSCALE)
@@ -11,43 +11,65 @@ sensor.skip_frames(time=2000)          # Let the camera adjust.
 net = None
 labels = None
 min_confidence = 1.0
+CO2_level = 0
+ventilated = True
 
-try:
-	# Load built in model
-	labels, net = tf.load_builtin_model('trained')
-except Exception as e:
-	raise Exception(e)
+def calculate_co2_level(people_count):
+	# This function should be called once every minute.
+	global CO2_level
+	CO2_level = CO2_level + (0.02556 * people_count)
+	return CO2_level
 
-colors = [ # Add more colors if you are detecting more than 7 types of classes at once.
-	(255,   0,   0),
-	(  0, 255,   0),
-	(255, 255,   0),
-	(  0,   0, 255),
-	(255,   0, 255),
-	(  0, 255, 255),
-	(255, 255, 255),
-]
+def main():
+	try:
+		# Load built in model
+		labels, net = tf.load_builtin_model('trained')
+	except Exception as e:
+		raise Exception(e)
 
-clock = time.clock()
-while(True):
-	clock.tick()
+	colors = [ # Add more colors if you are detecting more than 7 types of classes at once.
+		(255,   0,   0),
+		(  0, 255,   0),
+		(255, 255,   0),
+		(  0,   0, 255),
+		(255,   0, 255),
+		(  0, 255, 255),
+		(255, 255, 255),
+	]
 
-	img = sensor.snapshot()
+	clock = time.clock()
+	start = pyb.millis()
+	while(True):
+		clock.tick()
 
-	# detect() returns all objects found in the image (splitted out per class already)
-	# we skip class index 0, as that is the background, and then draw circles of the center
-	# of our objects
+		img = sensor.snapshot()
 
-	for i, detection_list in enumerate(net.detect(img, thresholds=[(math.ceil(min_confidence * 255), 255)])):
-		if (i == 0): continue # background class
-		if (len(detection_list) == 0): continue # no detections for this class?
+		people_count = 0
+		# detect() returns all objects found in the image (splitted out per class already)
+		# we skip class index 0, as that is the background, and then draw circles of the center
+		# of our objects
+		for i, detection_list in enumerate(net.detect(img, thresholds=[(math.ceil(min_confidence * 255), 255)])):
+			if (i == 0): continue # background class
+			if (len(detection_list) == 0): continue # no detections for this class?
 
-		print("********** %s **********" % labels[i])
-		for d in detection_list:
-			[x, y, w, h] = d.rect()
-			center_x = math.floor(x + (w / 2))
-			center_y = math.floor(y + (h / 2))
-			print('x %d\ty %d' % (center_x, center_y))
-			img.draw_circle((center_x, center_y, 12), color=colors[i], thickness=2)
+			print("********** %s **********" % labels[i])
 
-	print(clock.fps(), "fps", end="\n\n")
+			people_count = len(detection_list)
+			print("People Count = ", people_count)
+
+			for d in detection_list:
+				[x, y, w, h] = d.rect()
+				center_x = math.floor(x + (w / 2))
+				center_y = math.floor(y + (h / 2))
+				print('x %d\ty %d' % (center_x, center_y))
+				img.draw_circle((center_x, center_y, 12), color=colors[i], thickness=2)
+
+		# Calculate CO2 level.
+		if (pyb.elapsed_millis(start) >= 60000):
+			CO2_level = calculate_co2_level(people_count)
+			print("Approximate CO2 Level = " + str(CO2_level) + "ounce")
+			start = pyb.millis()
+
+		print(clock.fps(), "fps", end="\n\n")
+
+main()
